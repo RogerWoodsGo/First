f908aa80ba788c2699701d13ae037da78b574180
Wenchao Wu <wcwu@mail.ustc.edu.cn>
2014-10-31 22:50:16 +0000

    include file

---
+++ include/hello.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    return 0;
+}
